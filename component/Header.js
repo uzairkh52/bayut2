bd6f2f20c91cb5a692cb914443493633e27bc84a
muhammad uzair khan <uzairkh52@gmail.com>
2024-11-14 09:03:55 +0500

    tt

--- component/Header.js
+++ component/Header.js
@@ -9,33 +9,34 @@ const Header = () => {
   
   return (
     <>
-{/* <!-- Google tag (gtag.js) --> */}
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-4TRSZ5V876"></Script>
-<Script>
-{`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-4TRSZ5V876');
-`}
-</Script>
-
-{/* google tag manager */}
-{/* <!-- Google Tag Manager --> */}
-<Script>
-{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-58CC4PJ5');`}</Script>
-{/* <!-- End Google Tag Manager --> */}
 <Head>
-<meta name="google-site-verification" content="l556Qqvxm0WIu4TDeOY0EMYPtEAd6zYR0nS9JBMNWoo" />
+  <meta name="google-site-verification" content="l556Qqvxm0WIu4TDeOY0EMYPtEAd6zYR0nS9JBMNWoo" />
+  {/* <!-- Google tag (gtag.js) --> */}
+  <Script async src="https://www.googletagmanager.com/gtag/js?id=G-4TRSZ5V876"></Script>
+  <Script>
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-4TRSZ5V876');
+  `}
+  </Script>
+
+  {/* google tag manager */}
+  {/* <!-- Google Tag Manager --> */}
+  <Script>
+  {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-58CC4PJ5');`}</Script>
+  {/* <!-- End Google Tag Manager --> */}
 </Head>
 {/* <!-- Google Tag Manager (noscript) --> */}
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58CC4PJ5"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+height="0" width="0" className={styles.dNone + styles.hidden}></iframe></noscript>
+
 {/* <!-- End Google Tag Manager (noscript) --> */}
 
       {/* <Image src="../public/images/favicon.svg" alt="Vercel Logo" /> */}
